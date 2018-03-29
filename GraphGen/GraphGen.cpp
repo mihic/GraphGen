3166cbb93df1264c6d0f4fa2156d14b019c3b885
Miha Eleršič <miha.elersic@gmail.com>
2018-03-29 14:09:34 +0200

    Additional paramaters via CLI

--- GraphGen/GraphGen.cpp
+++ GraphGen/GraphGen.cpp
@@ -34,6 +34,11 @@ std::default_random_engine kRandomEngine;
 Graph generated_graph;
 bool kDebug = false;
 bool kDirected = true;
+int kScaleFreeInitialNodes = 2;
+int kScaleFreeMinDegree = 1;
+double kScaleFreeOffsetExponent = 1.0;
+int kMinCost = 1;
+int kMaxCost = 100;
 
 int main(int argc, const char* argv[]) {
   try {
@@ -48,10 +53,15 @@ int main(int argc, const char* argv[]) {
       ("s,seed", "Random generator seed, [int]", cxxopts::value<int>())
       ("k,centers", "Number of centers for pmed output, [int (1,n-1) ]")
       ("u,undirected", "Generate undirected graphs")
+      ("mincost","Minimum cost of edges", cxxopts::value<int>())
+      ("maxcost", "Maximum cost of edges", cxxopts::value<int>())
+      ("scalefree_initial_nodes", "Number of inital nodes in graph [int]", cxxopts::value<int>())
+      ("scalefree_min_degree", "Minimum degree of new nodes [int]", cxxopts::value<int>())
+      ("scalefree_offset_exponent", "Offset exponent applied to the probability of new edges [double]", cxxopts::value<double>())
       ;
-
     auto result = options.parse(argc, argv);
 
+    //Print help
     if (result.count("help")) {
       std::cout << options.help({ "" }) << std::endl;
       exit(0);
@@ -61,6 +71,7 @@ int main(int argc, const char* argv[]) {
       std::cerr << "debuging enabled" << std::endl;
     }
 
+    //Number of nodes
     if (result.count("nodes")) {
       kNumNodes = result["nodes"].as<int>();
       if (kNumNodes <= 0) {
@@ -76,6 +87,7 @@ int main(int argc, const char* argv[]) {
       exit(2);
     }
 
+    //Output format
     if (result.count("format")) {
       std::string format = result["format"].as<std::string>();
       if (kOutputFormatTypeMap.count(format)) {
@@ -94,6 +106,7 @@ int main(int argc, const char* argv[]) {
       exit(2);
     }
 
+    //Denstiy
     if (result.count("density")) {
       kDensity = result["density"].as<double>();
       if (kDensity <= 0 || kDensity > 1) {
@@ -109,6 +122,8 @@ int main(int argc, const char* argv[]) {
       exit(2);
     }
 
+
+    //Generator type
     if (result.count("type")) {
       std::string type = result["type"].as<std::string>();
       if (kGeneratorTypeMap.count(type)) {
@@ -127,6 +142,7 @@ int main(int argc, const char* argv[]) {
       exit(2);
     }
 
+    //Seed 
     if (result.count("seed")) {
       int seed = result["seed"].as<int>();
       kRandomEngine = std::default_random_engine(seed);
@@ -142,6 +158,8 @@ int main(int argc, const char* argv[]) {
         std::cerr << "Seed: " << random_seed << std::endl;
       }
     }
+
+    //Number of centers
     if (result.count("centers")) {
       int kNumCenters = result["centers"].as<int>();
       if (kNumCenters < 1 || kNumCenters >= kNumNodes) {
@@ -156,6 +174,8 @@ int main(int argc, const char* argv[]) {
       std::cerr << "centers: " << kNumCenters << std::endl;
     }
 
+
+    //Undirected
     if (result.count("undirected")) {
       kDirected = false;
     }
@@ -168,6 +188,37 @@ int main(int argc, const char* argv[]) {
       }
     }
 
+    //Cost of edges
+    if (result.count("mincost")) {
+      kMinCost = result["mincost"].as<int>();
+    }
+    if (result.count("maxcost")) {
+      kMaxCost = result["maxcost"].as<int>();
+    }
+    if (kDebug) {
+      std::cout << "mincost: " << kMinCost << std::endl;
+      std::cout << "maxcost: " << kMaxCost << std::endl;
+    }
+
+
+    // ScaleFree paramaters
+    if (kGenType == kScaleFree) {
+      if (result.count("scalefree_initial_nodes")) {
+        kScaleFreeInitialNodes = result["scalefree_initial_nodes"].as<int>();
+      }
+      if (result.count("scalefree_min_degree")) {
+        kScaleFreeMinDegree = result["scalefree_min_degree"].as<int>();
+      }     
+      if (result.count("scalefree_offset_exponent")) {
+        kScaleFreeOffsetExponent = result["scalefree_offset_exponent"].as<double>();
+      }
+      if (kDebug) {
+        std::cout << "scalefree_initial_nodes: " << kScaleFreeInitialNodes << std::endl;
+        std::cout << "scalefree_min_degree: " << kScaleFreeMinDegree << std::endl;
+        std::cout << "scalefree_offset_exponent: " << kScaleFreeOffsetExponent << std::endl;
+      }
+    }
+
   }
   catch (const cxxopts::OptionException& e) {
     std::cout << "error parsing options: " << e.what() << std::endl;
@@ -176,13 +227,13 @@ int main(int argc, const char* argv[]) {
 
   switch (kGenType) {
   case kRandom:
-    generated_graph = RandomGraph(kNumNodes, kRandomEngine, kDirected, kDensity, 1, 100);
+    generated_graph = RandomGraph(kNumNodes, kRandomEngine, kDirected, kDensity, kMinCost, kMaxCost);
     break;
   case kGrid:
-    generated_graph = Random2DGridGraph(kNumNodes, kRandomEngine, kDirected, kDensity, 1, 100);
+    generated_graph = Random2DGridGraph(kNumNodes, kRandomEngine, kDirected, kDensity, kMinCost, kMaxCost);
     break;
   case kScaleFree:
-    generated_graph = RandomScaleFreeGraph(kNumNodes,kRandomEngine,3,1.0,1,1,1);
+    generated_graph = RandomScaleFreeGraph(kNumNodes, kRandomEngine, kScaleFreeInitialNodes, kScaleFreeOffsetExponent, kScaleFreeMinDegree, kMinCost, kMaxCost);
     break;
   }
 
